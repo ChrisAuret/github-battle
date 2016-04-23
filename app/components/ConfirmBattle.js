ebf1beffbfc3eb8dd951146072199cf0e347c178
Chris Auret <Chris Auret>
2016-04-23 14:33:36 +1000

    Added Battle conatainer and component

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+function ConfirmBattle (props) {
+    
+  console.log(props);
+    
+  return props.isLoading === true
+    ? <p>LOADING</p>
+    : <p>CONFIRM BATTLE</p>
+}
+
+module.exports = ConfirmBattle;
