7c313baf614bb4a31ab0349a60002ac7042d14fe
Chris Auret <chris.auret@yahoo.co.uk>
2016-04-26 21:38:05 +1000

    Render battle results

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,12 +1,47 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-
-function dump (obj) {
-  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-}
+var styles = require('../styles');
+var UserDetails = require('../components/UserDetails');
+var UserDetailsWrapper = require('../components/UserDetailsWrapper');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 function Results (props) {
-  return <div>{dump(props)}</div>
+  
+  if(props.scores[0] === props.scores[1]){
+    return (
+      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>It's a tie!</h1>
+        <div className="col-sm-12" style={styles.space}>
+        <Link to="/playerOne">
+          <button type="button" className="btn btn-lg btn-danger"> Start over</button>
+        </Link>
+        </div>
+      </div>
+    )
+  }
+  
+  var winninIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winninIndex === 0 ? 1 : 0; 
+  
+  return (
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winninIndex]} info={props.playersInfo[winninIndex]}/>  
+        </UserDetailsWrapper> 
+        <UserDetailsWrapper header="Loser">
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]}/>  
+        </UserDetailsWrapper>
+      </div>
+      <div className="col-sm-12" style={styles.space}>
+        <Link to="/playerOne">
+          <button type="button" className="btn btn-lg btn-danger"> Start over</button>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 Results.propTypes = {
